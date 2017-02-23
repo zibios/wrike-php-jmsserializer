9b8e777e60d0040819f5b1981a03805b620646b3
zibios <zibios@gmail.com>
2017-02-23 18:28:18 +0100

    TransformerFactory introduction

---
+++ src/TransformerFactory.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+ * This file is part of the zibios/wrike-php-jmsserializer package.
+ *
+ * (c) Zbigniew Ślązak
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zibios\WrikePhpJmsserializer;
+
+use JMS\Serializer\SerializerInterface;
+use Zibios\WrikePhpJmsserializer\Transformer\Response\ResourceModelTransformer;
+use Zibios\WrikePhpJmsserializer\Transformer\Response\ResponseModelTransformer;
+
+/**
+ * Transformer Factory.
+ */
+class TransformerFactory
+{
+    /**
+     * @param SerializerInterface|null $serializer
+     *
+     * @return ResourceModelTransformer
+     */
+    public static function createResourceModelTransformer(SerializerInterface $serializer = null)
+    {
+        $serializer = self::prepareSerializer($serializer);
+
+        return new ResourceModelTransformer($serializer);
+    }
+
+    /**
+     * @param SerializerInterface|null $serializer
+     *
+     * @return ResponseModelTransformer
+     */
+    public static function createResponseModelTransformer(SerializerInterface $serializer = null)
+    {
+        $serializer = self::prepareSerializer($serializer);
+
+        return new ResponseModelTransformer($serializer);
+    }
+
+    /**
+     * @param SerializerInterface|null $serializer
+     *
+     * @return SerializerInterface
+     */
+    protected static function prepareSerializer(SerializerInterface $serializer = null)
+    {
+        if ($serializer === null) {
+            $serializer = SerializerFactory::createSerializer();
+        }
+
+        return $serializer;
+    }
+}
---
+++ tests/TransformerFactoryTest.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of the zibios/wrike-php-jmsserializer package.
+ *
+ * (c) Zbigniew Ślązak
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zibios\WrikePhpJmsserializer\Tests;
+
+use Zibios\WrikePhpJmsserializer\SerializerFactory;
+use Zibios\WrikePhpJmsserializer\Transformer\Response\ResourceModelTransformer;
+use Zibios\WrikePhpJmsserializer\Transformer\Response\ResponseModelTransformer;
+use Zibios\WrikePhpJmsserializer\TransformerFactory;
+use Zibios\WrikePhpLibrary\Transformer\ResponseTransformerInterface;
+
+/**
+ * Transformer Factory Test.
+ */
+class TransformerFactoryTest extends TestCase
+{
+    public function test_createResourceModelTransformer()
+    {
+        $transformer = TransformerFactory::createResourceModelTransformer();
+        self::assertInstanceOf(ResponseTransformerInterface::class, $transformer);
+        self::assertInstanceOf(ResourceModelTransformer::class, $transformer);
+
+        $serializer = SerializerFactory::createSerializer();
+        $transformer = TransformerFactory::createResourceModelTransformer($serializer);
+        self::assertInstanceOf(ResponseTransformerInterface::class, $transformer);
+        self::assertInstanceOf(ResourceModelTransformer::class, $transformer);
+    }
+
+    public function test_createResponseModelTransformer()
+    {
+        $transformer = TransformerFactory::createResponseModelTransformer();
+        self::assertInstanceOf(ResponseTransformerInterface::class, $transformer);
+        self::assertInstanceOf(ResponseModelTransformer::class, $transformer);
+
+        $serializer = SerializerFactory::createSerializer();
+        $transformer = TransformerFactory::createResponseModelTransformer($serializer);
+        self::assertInstanceOf(ResponseTransformerInterface::class, $transformer);
+        self::assertInstanceOf(ResponseModelTransformer::class, $transformer);
+    }
+}
